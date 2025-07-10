9805988c4c0199c23ba19947711e708e6022dca2
Betel Yohannes <betelyohannes39@gmail.com>
2025-07-11 02:30:01 +0300

    Task 1Add with_db_connection decorators

--- python-decorators-0x01/1-with_db_connection.py
+++ python-decorators-0x01/1-with_db_connection.py
@@ -1,25 +1,21 @@
 import sqlite3
 import functools
 
-# Decorator to manage DB connection
 def with_db_connection(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        conn = sqlite3.connect('users.db')  # Open DB
+        conn = sqlite3.connect('users.db')
         try:
-            result = func(conn, *args, **kwargs)  # Call original function with DB
-            return result
+            return func(conn, *args, **kwargs)
         finally:
-            conn.close()  # Always close the DB
+            conn.close()
     return wrapper
 
-# Function that fetches a user by ID, connection is passed automatically!
 @with_db_connection
 def get_user_by_id(conn, user_id):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
     return cursor.fetchone()
 
-# Run the function
 user = get_user_by_id(user_id=1)
 print(user)
