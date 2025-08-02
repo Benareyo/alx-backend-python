cb0bd1d1c30029b6a364e76eb58283dcd42e96fb
Betel Yohannes <betelyohannes39@gmail.com>
2025-08-02 03:35:02 +0300

    updating messaging/models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,8 @@
+# messaging/models.py
+
 from django.db import models
 from django.conf import settings
+
 
 class Message(models.Model):
     sender = models.ForeignKey(
@@ -15,8 +18,24 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    # Track if message was edited
+    edited = models.BooleanField(default=False)
+
     def __str__(self):
         return f"From {self.sender.username} to {self.receiver.username}"
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        related_name='history'
+    )
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of message {self.message.id} at {self.edited_at}"
 
 
 class Notification(models.Model):
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
@@ -9,3 +9,24 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if not instance.pk:
+        # New message, no old content to save
+        return
+
+    try:
+        old_message = Message.objects.get(pk=instance.pk)
+    except Message.DoesNotExist:
+        return  # If message does not exist yet, skip
+
+    if old_message.content != instance.content:
+        # Content changed: log old content in history
+        MessageHistory.objects.create(
+            message=instance,
+            old_content=old_message.content
+        )
+        # Mark message as edited
+        instance.edited = True
